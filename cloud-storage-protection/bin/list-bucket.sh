8b65cad60ea591f201748e44036c2202c7047c3e
Gabe Alford <redhatrises@gmail.com>
2025-08-25 12:36:48 -0600

    Remove deprecated QS integration (#43)

--- cloud-storage-protection/bin/list-bucket.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gsutil ls BUCKET
--- cloud-storage-protection/bin/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Uploading test files, please wait..."
-for i in $(ls TESTS_DIR)
-do
-    echo "Uploading $i to BUCKET..."
-    gsutil -q cp TESTS_DIR/$i BUCKET
-done
-echo "Upload complete. Check Cloud Functions logs or use the get-findings command for scan results."
